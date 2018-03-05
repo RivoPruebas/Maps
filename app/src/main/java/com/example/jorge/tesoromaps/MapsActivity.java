1de770430fb0dff82fe4790100da61f3ee5cb348
JorgeRiveiro <jorge.riveiro.romero@gmail.com>
2018-03-05 17:28:07 +0100

    first commit

---
+++ app/src/main/java/com/example/jorge/tesoromaps/MapsActivity.java
@@ -0,0 +1,77 @@
+package com.example.jorge.tesoromaps;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import com.google.android.gms.maps.model.MapStyleOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps_b);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        /* Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));*/
+        //Guardamos coordenadas en una variable
+        LatLng telePizza=new LatLng(42.236976, -8.712651);
+        LatLng laFayette=new LatLng(42.237441, -8.714267);
+        LatLng laPepita=new LatLng(42.237491, -8.716355);
+        //Creamos un marcador en las coordenadas anteriores
+        mMap.addMarker(new MarkerOptions().position(telePizza).title("Tesoro 1"));
+        mMap.addMarker(new MarkerOptions().position(laFayette).title("Tesoro 2"));
+        mMap.addMarker(new MarkerOptions().position(laPepita).title("Tesoro 3"));
+
+
+        //Moveremos la camara a esa posicion
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(telePizza,15));
+
+        //Elegir el tipo de mapa
+        mMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(this, R.raw.style_json));
+        //activar brujula
+        mMap.getUiSettings().setCompassEnabled(true);
+        //Comprobamos los permisos de localizacion y se los pedimos en caso de no tenerlos antes de activar MyLocation
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+        } else{
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION},1);
+        }
+
+    }
+}
